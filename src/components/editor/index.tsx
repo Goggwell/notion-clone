0a96d1115592d39689ac8966cb18fca85a33cd61
samuel-bd <samuely@frisseblikken.com>
2023-10-10 00:47:28 +0700

    feat(editor): repositioning to fit sidebar

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -9,5 +9,19 @@ export default function Editor() {
     content: "<p>Hello World!</p>",
   });
 
-  return <EditorContent editor={editor} />;
+  return (
+    <div
+      onClick={() => {
+        editor?.chain().focus().run();
+      }}
+      className="relative flex min-h-screen w-full cursor-text flex-col items-center p-32"
+    >
+      <div className="absolute left-8 top-8 rounded-lg bg-gray-100 px-2 py-1 text-sm text-gray-400">
+        Saved {/* {saveStatus} */}
+      </div>
+      <div className="relative w-full max-w-screen-lg">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
 }
